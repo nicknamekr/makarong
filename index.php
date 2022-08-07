3c64d0ef6bb253b0cda4d2f333e95d75fe5ac17c
Nickname <47962021+nicknamekr@users.noreply.github.com>
2022-08-07 06:51:05 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Team Makarong</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<a href="index.html" class="logo"><strong>Team</strong> <span style="color: white;">Makarong</span></a>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner" class="major">
+						<div class="inner">
+							<header class="major">
+								<h1>팀 마카롱에 오신걸 환영해요.</h1>
+							</header>
+							<div class="content" style="font-family: 'Pretendard' !important;">
+								<p>Discord 봇을 만들고 있는 개발 팀입니다. 편안하게 웹사이트를 봐주시길 바래요.</p>
+								<ul class="actions">
+									<li><a href="https://discord.gg/utWfjj9Jvd" class="button next scrolly">디스코드 서버</a></li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- One -->
+							<section id="one" class="tiles">
+								<article>
+									<span class="image">
+										<img src="images/pic01.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>당신의 멋진 프로젝트</p>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/pic02.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>와 우리의 멋진</p>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/pic03.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>프로젝트인것 같아요</p>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/pic04.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>왜냐하면 저희는 팀 마카롱</p>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/pic05.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>이니까요. 팀에 들어오고 싶으면</p>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/pic06.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3><a href="landing.html" class="link">프로젝트</a></h3>
+										<p>얼마든지 지원해주세요.</p>
+									</header>
+								</article>
+							</section>
+
+						<!-- Two -->
+							<section id="two">
+								<div class="inner">
+									<header class="major">
+										<h2>직접 답변하는 팬 서비스</h2>
+									</header>
+									<p>디스코드 서버에 오시면 저희가 직접 답변해드려요! 그러니 빨리 서버에 접속하고 봇 사용과 공지, 문의 및 잡담 등을 해보세요! 언제나 기다리고 있습니다.</p>
+									<ul class="actions">
+										<li><a href="https://discord.gg/utWfjj9Jvd" class="button next">Discord 서버</a></li>
+									</ul>
+								</div>
+							</section>
+
+					</div>
+
+				
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<ul class="copyright">
+                <li>Team Makarong은 Discord 봇 개발 팀입니다.</li>
+								<li>&copy; 2022 Team Makarong & Nickname 일부 권리 소유</li><li>디자인 및 템플릿 출처: <a href="https://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
